5ff59f85fc94899c4dc0fa9a9401bb63aa2e8020
Daniel Watson <danielwatson3141@gmail.com>
2020-01-18 11:55:18 -0500

    new motor controller setup

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -25,6 +25,6 @@ public final class Main {
    */
   public static void main(String... args) {
     RobotBase.startRobot(colorSensingRobot::new);
-    //RobotBase.startRobot(treadRobot::new);
+    //RobotBase.startRobot(Robot::new);
   }
 }
---
+++ src/main/java/frc/robot/otherRobot.java
@@ -0,0 +1,93 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+//import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.*;
+
+import edu.wpi.first.wpilibj.buttons.Trigger;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically
+ * it contains the code necessary to operate a robot with tank drive.
+ */
+public class otherRobot extends TimedRobot {
+  private DifferentialDrive m_myRobot;
+  private Joystick m_leftStick;
+  private Joystick m_rightStick;
+  private TalonSRX rTalon;
+  private VictorSPX rVex;
+  private TalonSRX lTalonOne;
+  private TalonSRX lTalonTwo;
+  private JoystickButton aButton;
+  private JoystickButton bButton;
+  private JoystickButton xButton;
+  private JoystickButton yButton;
+  private JoystickButton lBumper;
+  private JoystickButton rBumper;
+  private JoystickButton backButton;
+  private JoystickButton startButton;
+  private JoystickButton lStickPress;
+  private JoystickButton rStickPress;
+
+  @Override
+  public void robotInit() {
+    //m_myRobot = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+    m_leftStick = new Joystick(0);
+    m_rightStick = new Joystick(1);
+    rTalon = new WPI_TalonSRX(10);
+    rVex = new WPI_VictorSPX(0);
+    lTalonOne = new WPI_TalonSRX(1);
+    lTalonTwo = new WPI_TalonSRX(6);
+
+    // aButton = new JoystickButton(0);
+    // bButton = new JoystickButton(1);
+    // xButton = new JoystickButton(2);
+    // yButton = new JoystickButton(3);
+    // lBumper = new JoystickButton(4);
+    // rBumper = new JoystickButton(5);
+    // backButton = new JoystickButton(6);
+    // startButton = new JoystickButton(7);
+    // lStickPress = new JoystickButton(8);
+    // rStickPress = new JoystickButton(9);     
+
+    
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    //m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+
+    //if(m_leftStck.getRawButtonPressed{
+      //spin();
+    //}else{
+      // rTalon.set(ControlMode.PercentOutput, (m_rightStick.getY()+m_rightStick.getX())*.5);
+      // lVex.set(ControlMode.PercentOutput, (m_rightStick.getY()+m_rightStick.getX())*.5);
+      // lTalonOne.set(ControlMode.PercentOutput, (-m_rightStick.getY()+m_rightStick.getX())*.5);
+      // rTalonTwo.set(ControlMode.PercentOutput, (-m_rightStick.getY()+m_rightStick.getX())*.5);
+    //}
+    
+    
+  }
+
+  //make the robot spin 180 degrees once
+  // public void spin() {
+  //   //
+  //   lTalon.set(ControlMode.PercentOutput, .5);
+  //   rTalon.set(ControlMode.PercentOutput, .5);
+  // }
+}
