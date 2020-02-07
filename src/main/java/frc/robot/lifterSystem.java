cc423d3c7ff99c0704436919cd0d2abdbe438b1e
danielwatson <danielwatson3141@gmail.com>
2020-02-07 18:51:34 -0500

    performed motor controller inventory

--- src/main/java/frc/robot/lifterSystem.java
+++ src/main/java/frc/robot/lifterSystem.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -9,8 +10,8 @@ import edu.wpi.first.wpilibj.Talon;
 public class lifterSystem extends Subsystem {
 
     // This is the motors and hall effect sensors for the lifter mechanisms
-    private Talon extTalon;
-    private Talon ropeTalon;
+    private TalonSRX extTalon;
+    private TalonSRX ropeTalon;
     private DigitalInput sensorTop;
     private DigitalInput sensorBottom;
 
@@ -18,10 +19,10 @@ public class lifterSystem extends Subsystem {
         super(theRobot);
 
         // motor that extends arm
-        extTalon = new Talon(5);
+        extTalon = new TalonSRX(7);
 
         // motor that retracts rope
-        ropeTalon = new Talon(3);
+        ropeTalon = new TalonSRX(8);
 
         // sensors that tell us if we're at the top or bottom of the shaft
         sensorTop = new DigitalInput(0);
@@ -36,18 +37,18 @@ public class lifterSystem extends Subsystem {
 
     public void extend() {
         if (sensorTop.get()) {
-            extTalon.set(0);
+            extTalon.set(ControlMode.PercentOutput, 0);
         } else {
-            extTalon.set(.2);
+            extTalon.set(ControlMode.PercentOutput, .2);
         }
     }
 
     public void retract() {
-        ropeTalon.set(controller.getRawAxis(5));
+        ropeTalon.set(ControlMode.PercentOutput,controller.getRawAxis(5));
         if (sensorBottom.get()) {
-            extTalon.set(0);
+            extTalon.set(ControlMode.PercentOutput,0);
         } else {
-            extTalon.set(-.2);
+            extTalon.set(ControlMode.PercentOutput,-.2);
         }
 
     }
